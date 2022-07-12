c4cc4eae8e4a67e8c6518120e236ff96ca6ff2cf
sophiedamelio <sophiedamelio@gmail.com>
2022-07-12 11:45:45 -0700

    add final code for lab part A

--- App.js
+++ App.js
@@ -1,13 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import db from "./firebase";
+import firebase from "@firebase/app"; 
+
+import React, { useState, useEffect, useCallback } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat'
+
 
 export default function App() {
+  const [messages, setMessages] = useState([]);
+
+  const onSend = useCallback((messages = []) => {
+
+    db.collection("Chats")
+    .doc("myfirstchat")
+    .update({
+        // arrayUnion appends the message to the existing array
+        messages: firebase.firestore.FieldValue.arrayUnion(messages[0]),
+    });
+    setMessages((previousMessages) =>
+    GiftedChat.append(previousMessages, messages)
+    );
+  }, []);
+
+  useEffect(() => {
+    let unsubscribeFromNewSnapshots = db
+      .collection("Chats")
+      .doc("myfirstchat")
+      .onSnapshot((snapshot) => {
+        console.log("New Snapshot!");
+        setMessages(snapshot.data().messages);
+      });
+  
+    return function cleanupBeforeUnmounting() {
+      unsubscribeFromNewSnapshots();
+    };
+  }, []);
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GiftedChat
+      messages={messages}
+      onSend={messages => onSend(messages)}
+      user={{
+        _id: "1",
+        name: 'Sophie',
+        avatar: "https://i.imgur.com/8C9wiYe.png"
+      }}
+      placeholder={"please type something here..."}
+      showUserAvatar={true}
+      alwaysShowSend={true}
+      renderUsernameOnMessage={true}
+      />
   );
 }
 
